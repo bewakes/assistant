dcf55861e968ee5d7496541f87939e34f7741943
bewakes <spirit_bibek@yahoo.com>
2018-05-16 21:03:55 +0545

    add logging

---
+++ services/log.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    # create a file handler
+    handler = logging.FileHandler('/tmp/assistant.log')
+    handler.setLevel(logging.INFO)
+
+    # create a logging format
+    formatter = logging.Formatter('%(asctime)s %(name)s - %(levelname)s: %(message)s')  # noqa
+    handler.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(handler)
+
+    return logger
