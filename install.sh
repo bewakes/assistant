b96b2952908dbc26fd38a37fdd26197a5e6511d4
bewakes <spirit_bibek@yahoo.com>
2018-05-19 18:17:14 +0545

    make installer better; check dependencies

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+check_installed() {
+    echo -n Checking if $1 is installed..
+    $1 --version > /dev/null 2>/dev/null
+    if [[ $? != "0" ]]; then
+        echo NOT INSTALLED
+        echo
+        echo INSTALLATION FAILED: "'$1' required. Please install it and then re-run the installer."
+        exit 1
+    fi
+    echo INSTALLED
+    echo
+}
+
 clear_assistant_entry_bashrc() {
     cat ~/.bashrc | grep -vwE ASSISTANT_DIR > /tmp/bashrc
     # I don't know why direct writing to bashrc does not work
@@ -13,12 +26,19 @@ if [[ $1 == "-u" ]]; then
 else
     clear_assistant_entry_bashrc
     echo "You are now installing your very own Assistant..."
+    echo "CHECKING REQUIREMENTS..."
+    echo
+    # check netcat
+    check_installed netcat
+    # check vlc
+    check_installed vlc
+    # check youtube-dl
+    check_installed youtube-dl
 
-    echo "================================================="
     echo "Setting up Env Variable ASSISTANT_DIR..."
     echo "ASSISTANT_DIR=`pwd`" >> ~/.bashrc
+    echo
 
-    echo "Congratulations!! Your Assistant is now ready. Behave well with it. ;)"
-    echo "Exiting installer"
+    echo "CONGRATULATIONS!! Your Assistant is now ready. And you are more lazier. ;)"
 fi
-echo "NOTE: DON'T FORGET TO SOURCE YOUR BASHRC!!"
+echo "NOTE: Don't forget to source your bashrc"
